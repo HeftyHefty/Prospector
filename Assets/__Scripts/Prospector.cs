abcd9dfa84b0897e4ab446230846edd5c05d71aa
HeftyHefty <43052026+HeftyHefty@users.noreply.github.com>
2018-12-16 17:32:56 -0500

    Fixed scripts/added scripts

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -1,28 +1,109 @@
 ﻿using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
+public class Card : MonoBehaviour
+{
 
-public class Prospector : MonoBehaviour {
+    public string suit;
+    public int rank;
+    public Color color = Color.black;
+    public string colS = "Black";  // or "Red"
 
-	static public Prospector 	S;
+    public List<GameObject> decoGOs = new List<GameObject>();
+    public List<GameObject> pipGOs = new List<GameObject>();
 
-	[Header("Set in Inspector")]
-	public TextAsset			deckXML;
+    public GameObject back;  // back of card;
+    public CardDefinition def;  // from DeckXML.xml		
+    public SpriteRenderer[] spriteRenderers;
 
 
-	[Header("Set Dynamically")]
-	public Deck					deck;
+    // Use this for initialization
+    void Start()
+    {
+        SetSortOrder(0);
+    }
 
-	void Awake(){
-		S = this; //set up a Singleton for Prospector 
-	}
+    public void PopulateSpriteRenderers()
+    {
+        if (spriteRenderers == null || spriteRenderers.Length == 0)
+        {
+            spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+        }
+    }
 
-	void Start() {
-		deck = GetComponent<Deck> (); //Get the Deck
-		deck.InitDeck (deckXML.text); //Pass DeckXML to it 
-	}
+    public void SetSortingLayerName(string tSLN)
+    {
+        PopulateSpriteRenderers();
+        foreach (SpriteRenderer tSR in spriteRenderers)
+        {
+            tSR.sortingLayerName = tSLN;
+        }
+    }
 
+    public void SetSortOrder(int sOrd)
+    {
+        PopulateSpriteRenderers();
+        foreach (SpriteRenderer tSR in spriteRenderers)
+        {
+            if (tSR.gameObject == this.gameObject)
+            {
+                tSR.sortingOrder = sOrd;
+                continue;
+            }
+            switch (tSR.gameObject.name)
+            {
+                case "back":
+                    tSR.sortingOrder = sOrd + 2;
+                    break;
+                case "face":
+                default:
+                    tSR.sortingOrder = sOrd + 1;
+                    break;
+            }
+        }
+    }
+
+    public bool faceUp
+    {
+        get
+        {
+            return (!back.activeSelf);
+        }
+
+        set
+        {
+            back.SetActive(!value);
+        }
+    }
+
+    virtual public void OnMouseUpAsButton()
+    {
+        print(name);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+} // class Card
+
+[System.Serializable]
+public class Decorator
+{
+    public string type;         // For card pips, tyhpe = "pip"
+    public Vector3 loc;         // location of sprite on the card
+    public bool flip = false;   //whether to flip vertically
+    public float scale = 1.0f;
+}
+
+[System.Serializable]
+public class CardDefinition
+{
+    public string face; //sprite to use for face cart
+    public int rank;    // value from 1-13 (Ace-King)
+    public List<Decorator>
+                    pips = new List<Decorator>();  // Pips Used
 }
